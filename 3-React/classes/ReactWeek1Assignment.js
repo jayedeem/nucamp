373b490c0ef6ec421b256e23c03ea0405f040bc1
Andrew Cacayuran <andrew.cacayuran@MAC-CACAYURAN.local>
2020-09-05 12:26:06 -0700

    wk3 completed

--- 3-React/classes/ReactWeek1Assignment.js
+++ 3-React/classes/ReactWeek1Assignment.js
@@ -8,5 +8,22 @@ class Bootcamp {
   constructor(name, level, students = []) {
     (this.name = name), (this.level = level), (this.students = students);
   }
-  registerStudent(studentToRegister) {}
+  registerStudent(student) {
+    if (this.students.filter((s) => s.email === student.email).length) {
+      console.log(`The student ${student.email} already registered!`);
+    } else {
+      this.students.push(student);
+      console.log(`Registering ${student.email} to the bootcamp ${this.name} `);
+    }
+    return this.students;
+  }
 }
+
+const drew = new Student('Drew', 'drew@email.com', 'Irvine');
+const lucy = new Student('Lucy', 'Lucy@email.com', 'Irvine');
+const webDev = new Bootcamp('React', 'Intermediate');
+const express = new Bootcamp('Express', 'Intermediate');
+webDev.registerStudent(drew);
+express.registerStudent(lucy);
+webDev.registerStudent(drew);
+express.registerStudent(lucy);
