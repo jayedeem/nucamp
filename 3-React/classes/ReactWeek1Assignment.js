7b3fde03a4dd6a1fd08ee58bee7f24cfeadc998e
Andrew Cacayuran <andrew.cacayuran@MAC-CACAYURAN.local>
2020-09-05 09:45:23 -0700

    adding week3

---
+++ 3-React/classes/ReactWeek1Assignment.js
@@ -0,0 +1,12 @@
+class Student {
+  constructor(name, email, community) {
+    (this.name = name), (this.email = email), (this.community = community);
+  }
+}
+
+class Bootcamp {
+  constructor(name, level, students = []) {
+    (this.name = name), (this.level = level), (this.students = students);
+  }
+  registerStudent(studentToRegister) {}
+}
